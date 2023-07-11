0485e726bf79175e0d2b87136e529fdbf3743b9f
miklik1 <martin.miklik@brandmaster.com>
2023-07-11 13:50:31 +0200

    added in add to cart feature

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,18 @@
-import "./product-card.styles.scss";
+import { useContext } from 'react';
+
+import { CartContext } from '../../context/cart.context';
 
 import Button from "../button/button.component";
 
+import "./product-card.styles.scss";
+
+
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`$name`} />
@@ -11,7 +20,7 @@ const ProductCard = ({ product }) => {
         <div className="name">{name}</div>
         <div className="price">{price}</div>
       </div>
-      <Button buttonType="inverted">Add to card</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>Add to card</Button>
     </div>
   );
 };
