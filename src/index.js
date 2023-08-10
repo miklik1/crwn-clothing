05021ea72285dea76b7ebbb28272b9ece047e78e
miklik1 <martin.miklik@brandmaster.com>
2023-08-10 15:54:52 +0200

    start of redux implementation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
 
-import "./index.scss";
 import App from "./App";
 import { UserProvider } from "./context/user.context";
 import { CategoriesProvider } from "./context/categories.context";
 import { CartProvider } from "./context/cart.context";
+
+import { store } from './store/store'
+
 import reportWebVitals from "./reportWebVitals";
+
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <UserProvider>
         <CategoriesProvider>
@@ -21,6 +27,7 @@ root.render(
         </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
